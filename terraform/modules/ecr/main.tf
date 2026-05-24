f3b21dda31fd4669858a4328779caa744be9a5f8
Dennislee:) <55692615+dennislee928@users.noreply.github.com>
2026-05-24 13:08:56 +0800

    Auto-commit: Project implementation in progress

--- terraform/modules/ecr/main.tf
+++ terraform/modules/ecr/main.tf
@@ -1,37 +1,72 @@
-resource "aws_ecr_repository" "main" {
-  for_each = var.repositories
+# ECR Module
+terraform {
+  required_version = ">= 1.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
 
-  name                 = "${var.name_prefix}/${each.key}"
-  image_tag_mutability = lookup(each.value, "image_tag_mutability", "MUTABLE")
+variable "repository_name" {
+  description = "Name of the ECR repository"
+  type        = string
+}
 
+variable "image_tag_mutability" {
+  description = "Tag mutability setting"
+  type        = string
+  default     = "MUTABLE"
+}
+
+variable "scan_on_push" {
+  description = "Whether to scan images on push"
+  type        = bool
+  default     = true
+}
+
+variable "lifecycle_policy" {
+  description = "Lifecycle policy JSON"
+  type        = string
+  default     = ""
+}
+
+variable "tags" {
+  description = "Tags to apply to the repository"
+  type        = map(string)
+  default     = {}
+}
+
+# ECR Repository
+resource "aws_ecr_repository" "this" {
+  name                 = var.repository_name
+  image_tag_mutability = var.image_tag_mutability
   image_scanning_configuration {
-    scan_on_push = lookup(each.value, "scan_on_push", true)
+    scan_on_push = var.scan_on_push
   }
 
-  encryption_configuration {
-    encryption_type = lookup(each.value, "encryption_type", "AES256")
-    kms_key         = lookup(each.value, "kms_key_arn", null)
-  }
-
-  tags = merge({
-    Name = "${var.name_prefix}/${each.key}"
-  }, lookup(each.value, "tags", {}))
+  tags = var.tags
 }
 
-resource "aws_ecr_lifecycle_policy" "main" {
-  for_each = {
-    for k, v in var.repositories : k => v if lookup(v, "lifecycle_policy", null) != null
-  }
-
-  repository = aws_ecr_repository.main[each.key].name
-  policy     = each.value.lifecycle_policy
+# Lifecycle Policy (if provided)
+resource "aws_ecr_lifecycle_policy" "this" {
+  count       = var.lifecycle_policy != "" ? 1 : 0
+  repository  = aws_ecr_repository.this.name
+  policy      = var.lifecycle_policy
 }
 
-resource "aws_ecr_repository_policy" "main" {
-  for_each = {
-    for k, v in var.repositories : k => v if lookup(v, "policy", null) != null
-  }
+output "repository_url" {
+  description = "URL of the ECR repository"
+  value       = aws_ecr_repository.this.repository_url
+}
 
-  repository = aws_ecr_repository.main[each.key].name
-  policy     = each.value.policy
+output "repository_arn" {
+  description = "ARN of the ECR repository"
+  value       = aws_ecr_repository.this.arn
+}
+
+output "repository_name" {
+  description = "Name of the ECR repository"
+  value       = aws_ecr_repository.this.name
 }
